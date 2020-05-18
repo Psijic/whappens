7d2d8776e1f68e2078435745e80b26bc174ef04b
Viacheslav <psijic@gmail.com>
2020-05-18 17:27:55 +0300

    dao update

--- app/src/main/java/com/psvoid/whappens/data/MarkerDao.kt
+++ app/src/main/java/com/psvoid/whappens/data/MarkerDao.kt
@@ -8,21 +8,21 @@ import androidx.room.*
 interface MarkerDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(marker: ClusterMarker)
+    suspend fun insert(marker: ClusterMarker)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(markers: List<ClusterMarker>)
+    suspend fun insert(markers: List<ClusterMarker>)
 
     /**
      * Update a row with a value already set in a column, replace the old value with the new one.
      * @param markers new value to write
      */
     @Update
-    fun update(markers: List<ClusterMarker>)
+    suspend fun update(markers: List<ClusterMarker>)
 
     /** Select and returns the row that matches the key. */
     @Query("SELECT * from markers_table WHERE id = :key")
-    fun get(key: String): ClusterMarker?
+    fun get(key: String): LiveData<ClusterMarker>
 
     /** Delete all values from the table. This does not delete the table. */
     @Query("DELETE FROM markers_table")
