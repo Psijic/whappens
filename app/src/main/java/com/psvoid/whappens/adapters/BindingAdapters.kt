15bda333ec0b755d2e03e601edc5244b8ec47f1d
Viacheslav <psijic@gmail.com>
2020-06-14 18:01:06 +0300

    bottom bar images add

--- app/src/main/java/com/psvoid/whappens/adapters/BindingAdapters.kt
+++ app/src/main/java/com/psvoid/whappens/adapters/BindingAdapters.kt
@@ -1,8 +1,12 @@
 package com.psvoid.whappens.adapters
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.psvoid.whappens.R
 
 // Expressions: https://developer.android.com/topic/libraries/data-binding/expressions
 
@@ -13,7 +17,16 @@ fun bindIsGone(view: View, isGone: Boolean) {
 }
 
 @BindingAdapter("bottomSheetState")
-fun bindingBottomSheet(container: View, state: Int) {
+fun bindBottomSheetState(container: View, state: Int) {
     val behavior = BottomSheetBehavior.from(container)
     behavior.state = if (state == 0) BottomSheetBehavior.STATE_HIDDEN else state
+}
+
+@BindingAdapter(value = ["imageFromUrl", "category"], requireAll = false)
+fun bindImageFromUrl(view: ImageView, imageUrl: String?, category: String?) {
+    Glide.with(view)
+        .load(imageUrl)
+        .placeholder(R.drawable.discoveries)
+        .transition(DrawableTransitionOptions.withCrossFade())
+        .into(view)
 }
