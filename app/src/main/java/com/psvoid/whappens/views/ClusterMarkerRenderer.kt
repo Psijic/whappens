f26cc9766839885ed45a7d62dbf80da936030623
Viacheslav <psijic@gmail.com>
2020-06-23 23:32:03 +0300

    selected marker changes color

--- app/src/main/java/com/psvoid/whappens/views/ClusterMarkerRenderer.kt
+++ app/src/main/java/com/psvoid/whappens/views/ClusterMarkerRenderer.kt
@@ -1,16 +1,13 @@
 package com.psvoid.whappens.views
 
 import android.content.Context
-import android.widget.ImageView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
-import com.google.maps.android.ui.IconGenerator
-import com.psvoid.whappens.R
-import com.psvoid.whappens.data.Categories
 import com.psvoid.whappens.data.ClusterMarker
 import com.psvoid.whappens.viewmodels.MapViewModel
 
@@ -24,86 +21,22 @@ class ClusterMarkerRenderer(
     clusterManager: ClusterManager<ClusterMarker>,
     private val viewModel: MapViewModel
 ) : DefaultClusterRenderer<ClusterMarker>(context, map, clusterManager) {
-    private val iconGenerator = IconGenerator(context)
-
-    //    val clusterIconGenerator = IconGenerator(context)
-    private var imageView: ImageView = ImageView(context)
-
-    //    private var clusterImageView: ImageView
-
-    init {
-//        val multiProfile: View = LayoutInflater().inflate(R.layout.multi_profile, null)
-//        mClusterIconGenerator.setContentView(multiProfile)
-//        clusterImageView = multiProfile.findViewById(R.id.image)
-
-//        val dimension = resources.getDimension(R.dimen.custom_profile_image).toInt()
-//        imageView.layoutParams = ViewGroup.LayoutParams(dimension, dimension)
-//        val padding = resources.getDimension(R.dimen.custom_profile_padding).toInt()
-//        imageView.setPadding(padding, padding, padding, padding)
-//        iconGenerator.setContentView(imageView)
-
-    }
-
-//    override fun getColor(clusterSize: Int): Int {
-//        return Color.parseColor("#fa5788")
-//
-//
-//    }
-
-//    override fun onBeforeClusterItemRendered(marker: ClusterMarker, markerOptions: MarkerOptions) {
-//        loadImage(marker)
-//
-//        // Draw a single marker - show their profile photo and set the info window to show their name
-//        markerOptions
-//            .icon(getItemIcon(marker))
-////            .icon(loadImage(marker))
-//            .title(marker.title)
-//    }
 
     override fun onBeforeClusterItemRendered(item: ClusterMarker, markerOptions: MarkerOptions) {
-        val markerDescriptor = BitmapDescriptorFactory.defaultMarker(item.getCategoryColor())
-        markerOptions.icon(markerDescriptor)
+        markerOptions.icon(getMarkerColor(item))
     }
 
-//    override fun onClusterItemUpdated(item: ClusterMarker, marker: Marker) {
-//        val markerDescriptor = BitmapDescriptorFactory.defaultMarker(getMarkerColor(item))
-//        marker.setIcon(markerDescriptor)
-//    }
-
-    private fun getMarkerColor(item: ClusterMarker): Float {
-        //        val color = if (item.id == viewModel.selectedEvent.value?.id) BitmapDescriptorFactory.HUE_CYAN
-        return Categories.getCategory(item).color
+    override fun onClusterItemUpdated(item: ClusterMarker, marker: Marker) {
+        marker.setIcon(getMarkerColor(item))
     }
 
+    private fun getMarkerColor(item: ClusterMarker): BitmapDescriptor {
+        val color = if (item.id == viewModel.selectedEvent.value?.id) BitmapDescriptorFactory.HUE_ORANGE
+        else item.getCategoryColor()
+        return BitmapDescriptorFactory.defaultMarker(color)
 
-
-
-    /**
-     * Get a descriptor for a single marker (marker outside a cluster) for a marker icon
-     * @param marker ClusterMarker to return an BitmapDescriptor for
-     * @return the marker's profile photo as a BitmapDescriptor
-     */
-    private fun getItemIcon(marker: ClusterMarker): BitmapDescriptor {
-        imageView.setImageResource(R.drawable.john) //TODO: change to image
-//        mImageView.setImageResource(marker.image)
-        val icon = iconGenerator.makeIcon()
-        return BitmapDescriptorFactory.fromBitmap(icon)
+//        return if (item.id == viewModel.selectedEvent.value?.id)
+//            BitmapDescriptorFactory.fromResource(R.drawable.ic_location_on_24)
+//        else BitmapDescriptorFactory.defaultMarker(item.getCategoryColor())
     }
-
-    private fun loadImage(marker: ClusterMarker) {
-
-        //TODO: finish
-//        val imgUrl = marker.images?.thumb?.url
-//        imgUrl?.let {
-//            val imgUri = imgUrl.toUri().buildUpon().scheme("https").build() // Make image Uri
-//            Glide.with(context)
-//                .load(imgUri)
-//                .centerCrop()
-//                .apply(RequestOptions().placeholder(R.drawable.loading_animation).error(R.drawable.ic_broken_image))
-//                .into(imageView)
-//
-//        }
-    }
-
-
 }
